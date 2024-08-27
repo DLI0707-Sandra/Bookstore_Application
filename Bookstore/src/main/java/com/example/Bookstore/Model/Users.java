ddde33576ab5a1914fc3ec2eced2c505e487f0e9
Sandra Davis <Sandra.Davis@godigit.com>
2024-08-27 18:38:01 +0530

    Modified users model

--- Bookstore/src/main/java/com/example/Bookstore/Model/Users.java
+++ Bookstore/src/main/java/com/example/Bookstore/Model/Users.java
@@ -38,9 +38,8 @@ public class Users {
     @Column(name = "created_at")
     private LocalDateTime created_at;
 
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private Users users;
+    @OneToOne(mappedBy = "users")
+    private CustomerDetails customerDetails;
 
     @OneToOne(mappedBy = "users", cascade = CascadeType.ALL)
     private Cart cart;
