a87f32f4c234a23950b48aacbc676e2c42d90e91
Sandra Davis <Sandra.Davis@godigit.com>
2024-08-27 12:28:47 +0530

    Product(book) controller, repository and service

--- Bookstore/src/main/java/com/example/Bookstore/Service/BookServiceImplementation.java
+++ Bookstore/src/main/java/com/example/Bookstore/Service/BookServiceImplementation.java
@@ -1,7 +1,20 @@
 package com.example.Bookstore.Service;
 
+import com.example.Bookstore.Model.Book;
+import com.example.Bookstore.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookServiceImplementation implements BookService{
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Override
+    public List<Book> getAll() {
+        return bookRepository.findAll();
+    }
 }
