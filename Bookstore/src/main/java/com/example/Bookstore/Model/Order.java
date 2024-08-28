f4242392cc7067c8accf7508dd59c9ffa94b3a87
Aishwarya K <Aishwarya.K@godigit.com>
2024-08-28 14:26:16 +0530

    Order and OrderItem model

--- Bookstore/src/main/java/com/example/Bookstore/Model/Order.java
+++ Bookstore/src/main/java/com/example/Bookstore/Model/Order.java
@@ -1,4 +1,38 @@
 package com.example.Bookstore.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    @Column(name = "total_amount")
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "order_status")
+    private OrderStatus orderStatus;
+
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+
+    @OneToMany (mappedBy = "order")
+    private List<OrderItem> orderItems;
+
 }
