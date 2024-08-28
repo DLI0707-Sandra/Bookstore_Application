784846f2b6e1161270f04065b56c5a198833464a
Sandra Davis <Sandra.Davis@godigit.com>
2024-08-28 14:12:45 +0530

    Cart and CartItem connection and controller

--- Bookstore/src/main/java/com/example/Bookstore/Model/Cart.java
+++ Bookstore/src/main/java/com/example/Bookstore/Model/Cart.java
@@ -1,35 +1,35 @@
-//package com.example.Bookstore.Model;
-//
-//import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-//import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "cart")
-//
-//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
-//public class Cart {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long id;
-//
-//    @OneToOne
-//    @JoinColumn(name = "user_id")
-//    private Users users;
-//
-//    @Column(name = "created_at")
-//    private LocalDateTime created_at;
-//
-//    @OneToMany(mappedBy = "cart")
-//    private List<CartItem> cartItems;
-//}
+package com.example.Bookstore.Model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cart")
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+
+    @OneToMany(mappedBy = "cart")
+    private List<CartItem> cartItems;
+}
