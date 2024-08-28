4e3b4538b3ba0a7d80b4dbbc10988a045c04d558
KV.Dhanunjaya Redddy <KV.DhanunjayaReddy@godigit.com>
2024-08-28 15:49:17 +0530

    WishlistItem Controller

--- Bookstore/src/main/java/com/example/Bookstore/Repository/WishlistRepository.java
+++ Bookstore/src/main/java/com/example/Bookstore/Repository/WishlistRepository.java
@@ -1,8 +1,15 @@
 package com.example.Bookstore.Repository;
 
+import com.example.Bookstore.Model.Cart;
 import com.example.Bookstore.Model.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+    @Query("SELECT w FROM Wishlist w WHERE w.users.id = :userId")
+    Optional<Wishlist> findByUserId(@Param("userId") Long userId);
 }
--- Bookstore/src/main/java/com/example/Bookstore/Service/WishlistItemService.java
+++ Bookstore/src/main/java/com/example/Bookstore/Service/WishlistItemService.java
@@ -1,15 +1,17 @@
 package com.example.Bookstore.Service;
 
 
+import com.example.Bookstore.Model.Wishlist;
 import com.example.Bookstore.Model.WishlistItems;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface WishlistItemService {
 
     List<WishlistItems> getAllWishlists();
-    String deleteWishlist(Long wishlistId);
 
+    void addwishlistItem(Long productId, Wishlist wishlist);
+
+    String deleteWishlistitem(Long wishlistId);
 
 }
